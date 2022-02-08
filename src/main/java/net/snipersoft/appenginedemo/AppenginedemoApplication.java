6eea292cb32c0a6e83b1dfea084fe9ae58066d97
Lukasz Bednorz <lukasz.bednorz@snipersoft.net>
2022-02-08 07:23:33 +0100

    new Spring Boot project

---
+++ src/main/java/net/snipersoft/appenginedemo/AppenginedemoApplication.java
@@ -0,0 +1,13 @@
+package net.snipersoft.appenginedemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppenginedemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppenginedemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/net/snipersoft/appenginedemo/AppenginedemoApplicationTests.java
@@ -0,0 +1,13 @@
+package net.snipersoft.appenginedemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppenginedemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
